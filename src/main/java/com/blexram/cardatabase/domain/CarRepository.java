dfbee791f1f8b92c1c16054c72ba973a0b37beec
gobash-blex <brian_ramos_14@yahoo.com>
2025-05-25 20:29:35 -0700

    Initial commit

---
+++ src/main/java/com/blexram/cardatabase/domain/CarRepository.java
@@ -0,0 +1,6 @@
+package com.blexram.cardatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+}
---
+++ src/main/java/com/blexram/cardatabase/domain/Owner.java
@@ -0,0 +1,52 @@
+package com.blexram.cardatabase.domain;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ownerid;
+
+    private String firstName, lastName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<Car> cars;
+
+    public Owner() {}
+
+    public Owner(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getOwnerid() {
+        return ownerid;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
